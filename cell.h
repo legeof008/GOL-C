8f00794f4168179c1d9b667da51b12af678658d3
dawidbieniek <77001857+dawidbieniek@users.noreply.github.com>
2021-03-10 12:08:56 +0100

    Zmiana typu char na uchar w strukturze Cell

--- cell.h
+++ cell.h
@@ -3,9 +3,9 @@
 
 typedef struct{
 	char type; // 0 - martwa; 1 - żywa; 2 - ściana;
-	signed char R;
-	signed char G;
-	signed char B;
+	unsigned char R;
+	unsigned char G;
+	unsigned char B;
 	char neighbor; // miedzy 0 a 8 sąsiadów
 }Cell;
 
@@ -23,8 +23,6 @@ Matrix *mx_alloc(int r, int c); // alokuje pamięć na Matrix
 Matrix *mx_read_from_file(char *filename); // Czyta z pliku
 
 void mx_print(Matrix *mx); // wypisuje jak macierz poglądowo
-
-void mx_cpy(Matrix *src, Matrix *dest); //Kopiowanie macierzy z zerowaniem src
 
 char mx_get_single_val(Matrix *mx, int r, int c, char type); // wydobycie pojedynczej cechy komórki, t- type, n - sąsiad
 
