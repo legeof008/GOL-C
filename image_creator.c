c2deb4646775c9c36cd8d9eddbf36bb220225cc8
nebraszka <majusxp@gmail.com>
2021-03-24 09:17:34 +0100

    Dodane png_creator do image_creator

--- image_creator.c
+++ image_creator.c
@@ -106,3 +106,128 @@ int save_as_bitmap(FILE* file, Matrix* matrix, int scale)
 
 	return 0;
 }
+
+int write_png_file(char *filename)
+{
+  FILE *fp = fopen(filename, "wb");
+  if (!fp)
+  {
+    fprintf(stderr, "[write_png_file] Nie udalo otworzyc sie pliku %s do zapisu\n", filename);
+    return -1;
+  }
+
+  png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  if (!png)
+  {
+    fprintf(stderr, "[write_png_file] png_create_write_struct: nie powiodlo sie\n");
+    return -2;
+  }
+
+  info = png_create_info_struct(png);
+  if (!info)
+  {
+    fprintf(stderr, "[write_png_file] png_create_info_struct: nie powiodlo sie\n");
+    return -3;
+  }
+
+  if (setjmp(png_jmpbuf(png)))
+  {
+    fprintf(stderr, "[write_png_file] wystapil blad podczas init_io\n");
+    return -4;
+  }
+
+  png_init_io(png, fp);
+
+  if (setjmp(png_jmpbuf(png)))
+  {
+    fprintf(stderr, "[write_png_file] blad w trakcie tworzenia naglowka\n");
+    return -5;
+  }
+
+  png_set_IHDR(
+    png,
+    info,
+    width, height,
+    bitDepth,
+    colorType,
+    PNG_INTERLACE_NONE,
+    PNG_COMPRESSION_TYPE_DEFAULT,
+    PNG_FILTER_TYPE_DEFAULT
+  );
+
+  png_write_info(png, info);
+  if (setjmp(png_jmpbuf(png)))
+  {
+    fprintf(stderr, "[write_png_file] blad przy zapisie bajtow\n");
+    return -6;
+  }
+
+  png_write_image(png, rowPointers);
+  if (setjmp(png_jmpbuf(png)))
+  {
+    fprintf(stderr, "[write_png_file] blad przy koncu zapisu\n");
+    return -7;
+  }
+
+  png_write_end(png, NULL);
+
+  for (unsigned int y = 0; y < height; y++)
+    free(rowPointers[y]);
+  free(rowPointers);
+
+  fclose(fp);
+}
+
+
+int process_png_file(Matrix *matrix, int scale)
+{
+  if (scale >= MAX_SCALE || scale < 1)
+  {
+    fprintf(stderr, "Nie mozna utworzyc obrazu (podana skala jest zla. Powinna miescic sie w (1, %d))\n", MAX_SCALE);
+    return -1;
+  }
+
+  width = scale * matrix->c;
+  height = scale * matrix->r;
+  bitDepth = 8;
+  colorType = PNG_COLOR_TYPE_RGB;
+
+  rowPointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
+  for (int y = 0; y < height; y++)
+  {
+    rowPointers[y] = (png_byte*)malloc(sizeof(png_byte) * width * 3); // SPRAWDZIC: ile tu trzeba wpisac? wczesniej bylo: png_get_rowbytes(png,info));
+  }
+
+  for (unsigned int y = 0; y < matrix->r; y++)
+  {
+    for (unsigned int x = 0; x < matrix->c; x++)
+    {
+      for (unsigned int sy = 0; sy < scale; sy++)
+      {
+        for (unsigned int sx = 0; sx < scale; sx++)
+        {
+          png_bytep px = &(rowPointers[height - 1 - (scale * y + sy)][(scale * x + sx) * 3]);
+
+          if (mx_get_single_val(matrix, y, x, 't') == 0)  // martwa komorka
+          {
+            for(int i = 0 ; i < 3 ; i++)
+              px[i] = 0;
+          }
+          else if (mx_get_single_val(matrix, y, x, 't') == 2) // sciana
+          {
+            px[0] = 0;
+            px[1] = 0;
+            px[2] = 255;
+          }
+          else
+          {
+            px[0] = mx_get_single_val(matrix, y, x, 'r');
+            px[1] = mx_get_single_val(matrix, y, x, 'g');
+            px[2] = mx_get_single_val(matrix, y, x, 'b');
+          }
+        }
+      }
+    }
+  }
+  return 0;
+}
--- image_creator.h
+++ image_creator.h
@@ -3,6 +3,18 @@
 
 #include "cell.h"
 
+int width;
+int height;
+png_byte colorType;
+png_byte bitDepth;
+png_bytep *rowPointers;
+png_structp png;
+png_infop info;
+
+int write_png_file(char *filename);
+
+int process_png_file();
+
 // Zapisuje matrix do pliku w postaci bitmapy; zwraca 0, gdy udalo sie zapisac obraz; zwraca -1, gdy nie udalo sie zapisac obrazu
 int save_as_bitmap(FILE* file, Matrix *matrix, int scale);
 
